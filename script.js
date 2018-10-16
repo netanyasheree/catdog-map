8a89a84ca678aa8749284cb2bc941669630e6d2c
Netanya Sheree <netanyafink@gmail.com>
2018-10-16 17:39:27 +0900

    inputted stats, got everything working properly

--- script.js
+++ script.js
@@ -1,62 +1,53 @@
 var winner;
-var createPolitician = function(name, partyColor) {
-	var politician = {};
-	politician.name = name;
-	politician.electionResults = null;
-	politician.partyColor = partyColor;
+var createPet = function(name, nameS, color) {
+	var pet = {};
+	pet.name = name;
+	pet.pollResults = null;
+	pet.color = color;
+	pet.nameS = nameS;
+	pet.totalStatesWon = 0;
 
-	politician.tallyVotes = function() {
-		this.totalVotes = 0;
-
-		for (var i = 0; i < this.electionResults.length; i++) {
-			this.totalVotes = this.totalVotes + this.electionResults[i];
-		}
-	};
-
-	return politician;
+	return pet;
 };
 
-//creating politicians passing in names and rgb party colors
-var sandra = createPolitician("Sandra Dee", [190, 183, 223]);
-var veronica = createPolitician("Veronica Mars", [119, 191, 163]);
+//creating pets passing in names and rgb colors
+var cat = createPet("Cat States", "cats", [190, 183, 223]);
+var dog = createPet("Dog States", "dogs", [119, 191, 163]);
 
-console.log("Sandra's color is " + sandra.partyColor);
-console.log("Veronica's color is " + veronica.partyColor);
 
-sandra.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
+cat.pollResults = [1,1,1,1,1.1,1.1,1.57,1.1,1.5,1.1,1,1.1,1.1,1.1,1.1,1.1,1,1,1,1.66,1.83,1.87,1.1,1.1,1,1,1,1.1,1.1,1.46,1.1,1,1.4,1,1.1,1.39,1,1.1,1.43,1.1,1,1.1,1,1,1.1,1.65,1.1,1.1,1,1.1,1.1];
 
-veronica.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
+dog.pollResults = [1.1,1,1.25,1.35,1,1,1,1,1,1,1.15,1,1,1,1,1,1.1,1.1,1.27,1,1,1,1,1,1.27,1.2,1.1,1,1,1,1,1.32,1,1.1,1,1,1.27,1,1,1,1.1,1,1.23,1.29,1,1,1,1,1.1,1,1];
 
-sandra.electionResults[9] = 1;
-veronica.electionResults[9] = 28;
 
-sandra.electionResults[4] = 17;
-veronica.electionResults[4] = 38;
-
-sandra.electionResults[43] = 11;
-veronica.electionResults[43] = 27;
-
-console.log("Sandra's revised count for Florida is " + sandra.electionResults[9]);
-
-console.log(veronica.electionResults);
+tallyStatesWon = function() {
+	for (var i = 0; i < cat.pollResults.length; i++) {
+		if (cat.pollResults[i] > dog.pollResults[i]) {
+			cat.totalStatesWon++;
+		} else if (cat.pollResults[i] < dog.pollResults[i]) {
+			dog.totalStatesWon++;
+		}
+	}
+ };
+ 
 
 var setStateResults = function(state) {
 
 	theStates[state].winner = null;
 
-	if (sandra.electionResults[state] > veronica.electionResults[state]) {
-		theStates[state].winner = sandra;
-	} else if (sandra.electionResults[state] < veronica.electionResults[state]) {
-		theStates[state].winner = veronica;
+	if (cat.pollResults[state] > dog.pollResults[state]) {
+		theStates[state].winner = cat;
+	} else if (cat.pollResults[state] < dog.pollResults[state]) {
+		theStates[state].winner = dog;
 	}
 
 	//changing states' colors based on winner
 	var stateWinner = theStates[state].winner;
 
 	if (stateWinner !== null) {
-		theStates[state].rgbColor = stateWinner.partyColor;
+		theStates[state].rgbColor = stateWinner.color;
 	} else {
-		theStates[state].rgbColor = [11, 32, 57];
+		theStates[state].rgbColor = [143, 171, 214];
 	}
 
 	//populate bottom table
@@ -70,50 +61,35 @@ var setStateResults = function(state) {
 	var stateAbbrev = header.children[1];
 	stateAbbrev.innerText = theStates[state].nameAbbrev;
 
-	var politician1 = stateResults.children[1].children[0];
-	politician1.children[0].innerText = sandra.name;
-	politician1.children[1].innerText = sandra.electionResults[state];
-
-	var politician2 = stateResults.children[1].children[1];
-	politician2.children[0].innerText = veronica.name;
-	politician2.children[1].innerText = veronica.electionResults[state];
-
-	var stateWinnerName = stateResults.children[1].children[2].children[1];
+	var stateWinnerName = stateResults.children[1].children[0].children[1];
 	if (stateWinner !== null) {
-		stateWinnerName.innerText = stateWinner.name;
+		stateWinnerName.innerText = stateWinner.nameS;
 	} else {
 		stateWinnerName.innerText = "DRAW";
 	}
 }
 
-//get total votes #s by tallying up votes
-sandra.tallyVotes();
-veronica.tallyVotes();
+// get total #s by tallying up number of pets
+tallyStatesWon();
 
-//console log total votes
-console.log(sandra.totalVotes);
-console.log(veronica.totalVotes);
 
 //declare winner
-if (sandra.totalVotes > veronica.totalVotes) {
-	winner = sandra.name;
-} else if (sandra.totalVotes < veronica.totalVotes) {
-	winner = veronica.name;
+if (cat.totalStatesWon > dog.totalStatesWon) {
+	winner = cat.nameS;
+} else if (cat.totalStatesWon < dog.totalStatesWon) {
+	winner = dog.nameS;
 } else {
 	winner = "a draw";
 }
-
-console.log("The results are in and it's " + winner + "!");
 
 
 //populate top table
 var countryResults = document.getElementById("countryResults");
 
-//make code following clean + neat!
-var row = countryResults.children[0].children[0];
+var row = countryResults.children[0].children[0]; //make code following clean + neat!
 
-row.children[0].innerText = sandra.name;
-row.children[1].innerText = sandra.totalVotes;
-row.children[2].innerText = veronica.name;
-row.children[3].innerText = veronica.totalVotes;
+row.children[0].innerText = cat.name;
+row.children[1].innerText = cat.totalStatesWon;
+row.children[2].innerText = dog.name;
+row.children[3].innerText = dog.totalStatesWon;
 row.children[5].innerText = winner;
